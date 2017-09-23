6fa089cd8bb7847800ae45eba3bf9fa13950cfae
DreamRedux <DreamRedux@users.noreply.github.com>
2017-09-23 09:34:48 -0400

    Implementing JDBC Attempt #1

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,5 +1,10 @@
 package hello;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,20 +13,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-       /** grab jdbc connection
-        * confirm it's not null
-        * if not null, print "true"
-        */
-    	/**
+    	Greeting retVal = null;   
+    	
+    	
+    	   //return DriverManager.getConnection(dbUrl, username, password);
+    	   try {
+    		   URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        	   String username = dbUri.getUserInfo().split(":")[0];
+        	   String password = dbUri.getUserInfo().split(":")[1];
+        	   String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+			Connection testConnection = DriverManager.getConnection(dbUrl, username, password);
+			retVal = new Greeting(counter.incrementAndGet(), "Successful Connection");
+    	   } catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			retVal = new Greeting(counter.incrementAndGet(), e.getMessage());
+    	   } catch (URISyntaxException uriEx) {
+    		uriEx.printStackTrace();
+    		retVal = new Greeting(counter.incrementAndGet(), uriEx.getMessage());
+    	   }
+
+
+    	   /*
     	 * select query in code
     	 * return statement
     	 */
-    	return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+    	return retVal;
     }
 }
